124a2151aae13fd5674698d030ae1680857d0b7f
yiningh <yiningh@email.com>
2013-02-12 14:18:34 -0500

    cleaned up

--- bouncing/bouncing.js
+++ bouncing/bouncing.js
@@ -1,25 +1,26 @@
-var xPos = 1;
-var yPos = 1;
-x = 0;
-y = 0;
+var xPos = 3;
+var yPos = 3;
+
+var x = 1;
+var y = 1;
 
 function update() {
 	var box = document.getElementById("box");
-    if(x >= document.documentElement.clientWidth-108){
-        xPos = -1 * (Math.random()+0.5);
+
+    if(x >= document.documentElement.clientWidth-108 || x <= 0){
+        xPos *= -1;
     }
-    if(x <= -1){ xPos = 1 * (Math.random()+0.5); }
-    if(y >= document.documentElement.clientHeight-108){
-        yPos = -1 * (Math.random()+0.5);
+
+    if(y >= document.documentElement.clientHeight-108 || y <= 0){
+        yPos *= -1;
     }
-    if(y <= -1){
-        yPos = 1 * (Math.random()+0.5);
-    }
-    x+=xPos;
-    y+=yPos;
+
+    x += xPos;
+    y += yPos;
+
 	box.style.marginLeft = x + "px";
 	box.style.marginTop = y + "px";
 }
 
 
-setInterval(update,1000/300); // 1000/30 means try to update 30 times a second.
+setInterval(update,1000/300); 
