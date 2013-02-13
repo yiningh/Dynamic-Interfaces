d4ec38a69ff19f20d79361018def5cf4e09d4764
yiningh <yiningh@email.com>
2013-02-13 00:23:15 -0500

    gravity added

--- bouncing/bouncing.js
+++ bouncing/bouncing.js
@@ -1,26 +1,39 @@
-var xPos = 3;
-var yPos = 3;
 
-var x = 1;
-var y = 1;
+function load(){
 
-function update() {
-	var box = document.getElementById("box");
+    var box = document.getElementById("box"); 
+    var boxH = document.getElementById("box").clientHeight;
 
-    if(x >= document.documentElement.clientWidth-108 || x <= 0){
-        xPos *= -1;
+    var xPos = 3;
+    var yPos = 0;
+
+    var x = 1;
+    var y = 0;
+
+    var grav = 0.1;
+
+    var width = document.documentElement.clientWidth;
+    var height = document.documentElement.clientHeight;
+
+    function update() {
+        yPos += grav;
+        x += xPos;
+        y += yPos;
+
+        if(x >= width-boxH || x <= 0){
+            xPos *= -1;
+        }
+
+        if(y >= (height-boxH) ){
+            y = height-boxH;
+            yPos *= -0.8;
+        }
+
+    	box.style.marginLeft = x + "px";
+    	box.style.marginTop = y + "px";
     }
 
-    if(y >= document.documentElement.clientHeight-108 || y <= 0){
-        yPos *= -1;
-    }
 
-    x += xPos;
-    y += yPos;
+    setInterval(update,1000/300); 
 
-	box.style.marginLeft = x + "px";
-	box.style.marginTop = y + "px";
 }
-
-
-setInterval(update,1000/300); 
