fac90ee7ec0210e015c59df4314ac73ee048fb61
yiningh <yiningh@email.com>
2013-03-28 15:27:12 -0400

    ''

--- chorus/www/js/script.js
+++ chorus/www/js/script.js
@@ -1,28 +1,31 @@
 var beat = -1;
-var bar = 0;
 var startTime;
-var tempo = 80; // BPM (beats per minute)
+var tempo = 80;
 var eighthNoteTime = (60 / tempo) / 2;
 
 /*--load sound sources--*/
-var ah01 = new sound('audio/ah01.wav');
-var ah02 = new sound('audio/ah02.wav');
-var ah03 = new sound('audio/ah03.wav');
-var ah04 = new sound('audio/ah04.wav');
-var ah05 = new sound('audio/ah05.wav');
-var aya01 = new sound('audio/aya01.wav');
-var aya02 = new sound('audio/aya02.wav');
-var aya03 = new sound('audio/aya03.wav');
-var aya04 = new sound('audio/aya04.wav');
-var aya05 = new sound('audio/aya05.wav');
+var ah01 = new sound('audio/max/ah01.wav');
+var ah02 = new sound('audio/max/ah02.wav');
+var ah03 = new sound('audio/max/ah03.wav');
+var ah04 = new sound('audio/max/ah04.wav');
+var ah05 = new sound('audio/max/ah05.wav');
+var aya01 = new sound('audio/max/ma01.wav');
+var aya02 = new sound('audio/max/ma02.wav');
+var aya03 = new sound('audio/max/ma03.wav');
+var aya04 = new sound('audio/max/ma04.wav');
+var aya05 = new sound('audio/max/ma05.wav');
+var dd01 = new sound('audio/max/dingdong01.wav');
+var dd02 = new sound('audio/max/dingdong02.wav');
+var dd03 = new sound('audio/max/dingdong03.wav');
+var dd04 = new sound('audio/max/dingdong04.wav');
+var dd05 = new sound('audio/max/dingdong05.wav');
+var ohno01 = new sound('audio/max/ohno01.wav');
+var ohno02 = new sound('audio/max/ohno02.wav');
+var ohno03 = new sound('audio/max/ohno03.wav');
+var ohno04 = new sound('audio/max/ohno04.wav');
+var ohno05 = new sound('audio/max/ohno05.wav');
 
-/*end of load sound source*/
-var soundClipsAh = [ah01, ah02, ah03, ah04, ah05];
-var ah01s, ah02s, ah03s, ah04s, ah05s;
-var soundClipsAhS = [ah01s, ah02s, ah03s, ah04s, ah05s];
-var soundClipsAya = [aya01, aya02, aya03. aya04, aya05];
-var aya01s, aya02s, aya03s, aya04s, aya05s;
-var soundClipsAyaS = [aya01s, aya02s, aya03s, aya04s, aya05s];
+var maxVoice = [ah05, ah04, ah03, ah02, ah01, aya05, aya04, aya03. aya02, aya01, dd05, dd04, dd03, dd02, dd01, ohno05, ohno04, ohno03, ohno02, ohno01];
 
 $(document).ready(function() {
     init();
@@ -47,56 +50,154 @@ function init() {
         accept: '#dragging-area article', 
         hoverClass: 'hovered',
         drop: function(event, ui) {
+            if ($(this).hasClass('clicked')||$(this).hasClass('clicked-two')||$(this).hasClass('clicked-three')||$(this).hasClass('clicked-four')){
+                $(this).removeClass('clicked');
+                $(this).removeClass('clicked-two')
+                $(this).removeClass('clicked-three')
+                $(this).removeClass('clicked-four')
+                for(var i = 0; i < 80; i++){
+                    if( this.id == ('pitch-'+ [i+1])){
+                        pitches[i] = false;
+                        pitches[80+i] = false;
+                        pitches[160+i] = false;
+                        pitches[240+i] = false;
+                    }
+                }
+            }
             if (ui.draggable.is('#voice-one')) {
                 $(this).addClass('clicked');
+                for(var i = 0; i < 80; i++){
+                    if( this.id == ('pitch-'+ [i+1])){pitches[i] = true;}
+                }
             } 
             if (ui.draggable.is('#voice-two')){
                 $(this).addClass('clicked-two');
+                for(var i = 0; i < 80; i++){
+                    if( this.id == ('pitch-'+ [i+1])){pitches[80+i] = true;}
+                }
             }
             if (ui.draggable.is('#voice-three')){
                 $(this).addClass('clicked-three');
+                for(var i = 0; i < 80; i++){
+                    if( this.id == ('pitch-'+ [i+1])){pitches[160+i] = true;}
+                }
             }
             if (ui.draggable.is('#voice-four')){
                 $(this).addClass('clicked-four');
+                for(var i = 0; i < 80; i++){
+                    if( this.id == ('pitch-'+ [i+1])){pitches[240+i] = true;}
+                }
+            }
+            /*=====================DROP SOUND: MAXIE DECODING=================*/
+            if(username == 'maxie'){
+                for(var i = 0; i < 320; i++){
+                    if(pitches[i] == true){max[i] = true;}
+                }
+                for(var i = 0; i < 320; i++){
+                    if(pitches[i] == false){max[i] = false;}
+                }
             }
         }
     });
 }
 
-function dropEvt(id){
-   alert(this.id);
-}   
-
 function playBeats() {
     startTime = audioContext.currentTime;
     var time = startTime;
-    console.log('in beat loop '+soundClipsAh[4])
-    for(var i = 0; i < 5; i++){
-        if(soundClipsAhS[i] == true){
-            soundClipsAh[i].play(time + 1 * eighthNoteTime);
-        }
-        if(soundClipsAyaS[i] == true){
-            soundClipsAya[i].play(time + 1 * eighthNoteTime);
-        }
+    /*======================= AH ===========================*/
+    for(var i =0; i < 5; i ++){
+        if(max[i] == true){maxVoice[i].play(time + 1 *eighthNoteTime);}
+        if(max[5+i] == true){maxVoice[i].play(time + 2 *eighthNoteTime);}
+        if(max[10+i] == true){maxVoice[i].play(time + 3 *eighthNoteTime);}
+        if(max[15+i] == true){maxVoice[i].play(time + 4 *eighthNoteTime);}
+        if(max[10+i] == true){maxVoice[i].play(time + 5 *eighthNoteTime);}
+        if(max[25+i] == true){maxVoice[i].play(time + 6 *eighthNoteTime);}
+        if(max[30+i] == true){maxVoice[i].play(time + 7 *eighthNoteTime);}
+        if(max[35+i] == true){maxVoice[i].play(time + 8 *eighthNoteTime);}
+        if(max[40+i] == true){maxVoice[i].play(time + 9 *eighthNoteTime);}
+        if(max[45+i] == true){maxVoice[i].play(time + 10 *eighthNoteTime);}
+        if(max[50+i] == true){maxVoice[i].play(time + 11 *eighthNoteTime);}
+        if(max[55+i] == true){maxVoice[i].play(time + 12 *eighthNoteTime);}
+        if(max[60+i] == true){maxVoice[i].play(time + 13 *eighthNoteTime);}
+        if(max[65+i] == true){maxVoice[i].play(time + 14 *eighthNoteTime);}
+        if(max[70+i] == true){maxVoice[i].play(time + 15 *eighthNoteTime);}
+        if(max[75+i] == true){maxVoice[i].play(time + 16 *eighthNoteTime);}
     }
+    /*======================= MA ===========================*/
+    for(var i =5; i < 10; i ++){
+        if(max[80+i] == true){maxVoice[i].play(time + 1 *eighthNoteTime);}
+        if(max[85+i] == true){maxVoice[i].play(time + 2 *eighthNoteTime);}
+        if(max[90+i] == true){maxVoice[i].play(time + 3 *eighthNoteTime);}
+        if(max[95+i] == true){maxVoice[i].play(time + 4 *eighthNoteTime);}
+        if(max[100+i] == true){maxVoice[i].play(time + 5 *eighthNoteTime);}
+        if(max[105+i] == true){maxVoice[i].play(time + 6 *eighthNoteTime);}
+        if(max[110+i] == true){maxVoice[i].play(time + 7 *eighthNoteTime);}
+        if(max[115+i] == true){maxVoice[i].play(time + 8 *eighthNoteTime);}
+        if(max[120+i] == true){maxVoice[i].play(time + 9 *eighthNoteTime);}
+        if(max[125+i] == true){maxVoice[i].play(time + 10 *eighthNoteTime);}
+        if(max[130+i] == true){maxVoice[i].play(time + 11 *eighthNoteTime);}
+        if(max[135+i] == true){maxVoice[i].play(time + 12 *eighthNoteTime);}
+        if(max[140+i] == true){maxVoice[i].play(time + 13 *eighthNoteTime);}
+        if(max[145+i] == true){maxVoice[i].play(time + 14 *eighthNoteTime);}
+        if(max[150+i] == true){maxVoice[i].play(time + 15 *eighthNoteTime);}
+        if(max[155+i] == true){maxVoice[i].play(time + 16 *eighthNoteTime);}
+    }
+    /*======================= DINGDONG ===========================*/
+    for(var i =10; i < 15; i ++){
+        if(max[160+i] == true){maxVoice[i].play(time + 1 *eighthNoteTime);}
+        if(max[165+i] == true){maxVoice[i].play(time + 2 *eighthNoteTime);}
+        if(max[170+i] == true){maxVoice[i].play(time + 3 *eighthNoteTime);}
+        if(max[175+i] == true){maxVoice[i].play(time + 4 *eighthNoteTime);}
+        if(max[180+i] == true){maxVoice[i].play(time + 5 *eighthNoteTime);}
+        if(max[185+i] == true){maxVoice[i].play(time + 6 *eighthNoteTime);}
+        if(max[190+i] == true){maxVoice[i].play(time + 7 *eighthNoteTime);}
+        if(max[195+i] == true){maxVoice[i].play(time + 8 *eighthNoteTime);}
+        if(max[200+i] == true){maxVoice[i].play(time + 9 *eighthNoteTime);}
+        if(max[205+i] == true){maxVoice[i].play(time + 10 *eighthNoteTime);}
+        if(max[210+i] == true){maxVoice[i].play(time + 11 *eighthNoteTime);}
+        if(max[215+i] == true){maxVoice[i].play(time + 12 *eighthNoteTime);}
+        if(max[220+i] == true){maxVoice[i].play(time + 13 *eighthNoteTime);}
+        if(max[225+i] == true){maxVoice[i].play(time + 14 *eighthNoteTime);}
+        if(max[230+i] == true){maxVoice[i].play(time + 15 *eighthNoteTime);}
+        if(max[235+i] == true){maxVoice[i].play(time + 16 *eighthNoteTime);}
+    }
+    /*======================= OHNO ===========================*/
+    for(var i =15; i < 20; i ++){
+        if(max[240+i] == true){maxVoice[i].play(time + 1 *eighthNoteTime);}
+        if(max[245+i] == true){maxVoice[i].play(time + 2 *eighthNoteTime);}
+        if(max[250+i] == true){maxVoice[i].play(time + 3 *eighthNoteTime);}
+        if(max[255+i] == true){maxVoice[i].play(time + 4 *eighthNoteTime);}
+        if(max[260+i] == true){maxVoice[i].play(time + 5 *eighthNoteTime);}
+        if(max[265+i] == true){maxVoice[i].play(time + 6 *eighthNoteTime);}
+        if(max[270+i] == true){maxVoice[i].play(time + 7 *eighthNoteTime);}
+        if(max[275+i] == true){maxVoice[i].play(time + 8 *eighthNoteTime);}
+        if(max[280+i] == true){maxVoice[i].play(time + 9 *eighthNoteTime);}
+        if(max[285+i] == true){maxVoice[i].play(time + 10 *eighthNoteTime);}
+        if(max[290+i] == true){maxVoice[i].play(time + 11 *eighthNoteTime);}
+        if(max[295+i] == true){maxVoice[i].play(time + 12 *eighthNoteTime);}
+        if(max[300+i] == true){maxVoice[i].play(time + 13 *eighthNoteTime);}
+        if(max[305+i] == true){maxVoice[i].play(time + 14 *eighthNoteTime);}
+        if(max[310+i] == true){maxVoice[i].play(time + 15 *eighthNoteTime);}
+        if(max[315+i] == true){maxVoice[i].play(time + 16 *eighthNoteTime);}
+    }
+
+
+
 }
 
 function stopBeat(){
-    beat = 18;
+    beat = 100;
 }
 
 function load(){
     beat = 1;
     playBeats();
     function addBeat(){
-        console.log("in load loop " +ah05s);
         beat += 1;
-        if (beat == 17){
-            beat = 1;
-            bar = 0;
+        if (beat >= 17 && beat <= 22){
             playBeats();
+            beat = 1;
         }
-        console.log('beat is: '+ beat);
         beatVisualizer();
     }
     setInterval(addBeat,eighthNoteTime*1000);
